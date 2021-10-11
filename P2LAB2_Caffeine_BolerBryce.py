ee76b7a1547ac99389e0cf8e9ab34a55afd3b88f
Bolerb600 <91216470+Bolerb600@users.noreply.github.com>
2021-10-11 12:52:44 -0400

    Add files via upload

---
+++ P2LAB2_Caffeine_BolerBryce.py
@@ -0,0 +1,9 @@
+caffeine_mg = float(input())
+
+six_hours = caffeine_mg / 2
+twelve_hours = six_hours / 2
+twentyfour_hours = twelve_hours / 4
+
+print('After 6 hours:', '{:.2f}'.format(six_hours),'mg')
+print('After 12 hours:', '{:.2f}'.format(twelve_hours),'mg')
+print('After 24 hours:', '{:.2f}'.format(twentyfour_hours),'mg')
